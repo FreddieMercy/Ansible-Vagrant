cb4101673f77d70b29ce360ebb1cd09f485d1d48
FreddieMercy <junhao.zhang.freddie@gmail.com>
2021-03-08 12:53:00 -0800

    all passed

--- shell-script.sh
+++ shell-script.sh
@@ -2,7 +2,7 @@
 
 yum -y install --quiet httpd httpd_devel
 # Copy configuration files.
-cp httpd.conf /etc/httpd/conf/httpd.conf
+cp /etc/httpd/conf/httpd.conf httpd.conf
 #cp httpd-vhosts.conf /etc/httpd/conf/httpd-vhosts.conf
 
 # Start Apache and configure it to run at boot
