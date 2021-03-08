f830986d89e3cc6f3fa270c32e6ed371021c34d1
FreddieMercy <junhao.zhang.freddie@gmail.com>
2021-03-08 12:26:29 -0800

    make playbook do the same thing

--- shell-script.sh
+++ shell-script.sh
@@ -5,6 +5,6 @@ yum -y install --quiet httpd httpd_devel
 cp httpd.conf /etc/httpd/conf/httpd.conf
 cp httpd-vhosts /etc/httpd/conf/httpd-vhosts.conf
 
-# start Apache and configure it to run at boot
+# Start Apache and configure it to run at boot
 service httpd start
 chkconfig httpd on
